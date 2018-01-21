2abdab0e42bc8917f56b1c6fb1b5c655f02d1f24
Pintoko Daruadi <pintoko.mpd@gmail.com>
2018-01-21 21:42:35 +0700

    remove sqlite structure

--- etc/create_table.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `rumah` (`id` int PRIMARY KEY,`nama` varchar(50) NOT NULL,`alamat` varchar(1000) NOT NULL,`kapasitas` int(11) NOT NULL);
-
-INSERT INTO `rumah` (`id`, `nama`, `alamat`, `kapasitas`) VALUES (1, 'Rumah Hijau', 'Jl. Al Ikhlas', 8);
